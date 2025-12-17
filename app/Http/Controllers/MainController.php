fe4ddd6936686887a5fef9062a24726f43461048
Rizki Nur <rizkinurikhsan@mail.ugm.ac.id>
2025-12-17 20:30:54 +0700

    Task G: Menyelesaikan implementasi chart dinamis untuk Gender dan Top 5 Pekerjaan

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pegawai;
+use App\Models\Pekerjaan;
+use Illuminate\Http\Request;
+
 class MainController extends Controller
 {
     public function index() {
-        return view('index');
+        // Data untuk Chart 1: Gender
+        $maleCount = Pegawai::where('gender', 'male')->count();
+        $femaleCount = Pegawai::where('gender', 'female')->count();
+
+        // Data untuk Chart 2: Top 5 Pekerjaan
+        $topPekerjaan = Pekerjaan::withCount('pegawai')
+            ->orderBy('pegawai_count', 'desc')
+            ->take(5)
+            ->get();
+
+        return view('index', [
+            'genderData' => [$maleCount, $femaleCount],
+            'pekerjaanLabels' => $topPekerjaan->pluck('nama'),
+            'pekerjaanValues' => $topPekerjaan->pluck('pegawai_count'),
+        ]);
     }
 }
